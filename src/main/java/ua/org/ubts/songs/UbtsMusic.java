8fca291efbdfa7233e9d9660fcd3a2b8057c22fe
Yaroslav Kuchkovskyi <y.kuchkovsky@gmail.com>
2019-09-13 17:42:38 +0300

    Rename project

--- src/main/java/ua/org/ubts/songs/UbtsMusic.java
+++ src/main/java/ua/org/ubts/songs/UbtsMusic.java
@@ -6,10 +6,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class MusicStoreApplication {
+public class UbtsMusic {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MusicStoreApplication.class, args);
+		SpringApplication.run(UbtsMusic.class, args);
 	}
 
 }
--- src/test/java/ua/org/ubts/songs/UbtsMusicTests.java
+++ src/test/java/ua/org/ubts/songs/UbtsMusicTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MusicStoreApplicationTests {
+public class UbtsMusicTests {
 
 	@Test
 	public void contextLoads() {
